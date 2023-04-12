8312e19369bffb33aaf16597e88ac8b70963b138
zy <000>
2023-04-12 17:08:58 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,16 @@
 module.exports = {
     title: 'Hello VuePress',  // 设置网站标题
-    description : 'VuePress',
+    description: 'VuePress',
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', {rel: 'icon', href: '/favicon.ico'}]
     ],
     base: '/notes/', // 和仓库名相同
-    dest:'dist', // 输出目录
-    themeConfig : {
-        nav : [
-            { text: "home", link: "/" },// ‘/’结束，默认读取 README.md
-            { text: "foo", link: "/guide/foo/" },
-            { text: "bar", link: "/guide/bar/" }
+    dest: 'dist', // 输出目录
+    themeConfig: {
+        nav: [
+            {text: "home", link: "/"},// ‘/’结束，默认读取 README.md
+            {text: "foo", link: "/guide/foo/"},
+            {text: "bar", link: "/guide/bar/"}
         ],
         sidebar: {
             // 不同的页面组来显示不同的侧边栏
